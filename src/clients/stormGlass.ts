8237947043108ebc77ce87a8802b1eb09a210e14
j4ck-7dev <j4ckson7dev@gmai.com>
2026-02-17 12:06:48 -0300

    Adicionando template para authorization no headers

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -44,8 +44,14 @@ export class StormGlass {
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> { // É recomendado que todas as funções sejam tipadas, dizendo o tipo dos argumentos e o tipo de retorno.
         const response = await this.request.get<StormGlassForecastResponse>(
-            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`) 
-            return this.normalizeResponse(response.data) // Sempre use o return 
+            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
+            {
+                headers: {
+                    Authorization: 'fake-token'
+                }
+            }
+        ) 
+        return this.normalizeResponse(response.data) // Sempre use o return 
     }
 
     private normalizeResponse(
